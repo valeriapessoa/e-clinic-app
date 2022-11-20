ae7d8718f7ecfa27ade915c5a9014c243799fb25
Valeria Pessoa <65936312+valeriapessoa@users.noreply.github.com>
2022-11-20 12:02:55 -0300

    Projeto App E-Clinic

---
+++ Banco de Dados/e-clinic.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 12-Nov-2022 às 19:18
+-- Versão do servidor: 10.4.21-MariaDB
+-- versão do PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `e-clinic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `consultas`
+--
+
+CREATE TABLE `consultas` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `celular` varchar(15) NOT NULL,
+  `convenio` varchar(50) NOT NULL,
+  `carteira` varchar(20) NOT NULL,
+  `unidade` varchar(50) NOT NULL,
+  `especialidade` varchar(50) NOT NULL,
+  `dtconsulta` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `consultas`
+--
+
+INSERT INTO `consultas` (`id`, `nome`, `celular`, `convenio`, `carteira`, `unidade`, `especialidade`, `dtconsulta`) VALUES
+(16, 'Rebeca Moreira', '(11) 99322-1695', 'One Health', '0 590 599562662662 6', 'Morumbi', 'Ortopedia e Traumatologia', '26/11/2022 19:59'),
+(17, 'Gabriel Roque', '(11) 99886-2316', 'Unimed', '0 123 456789012345 6', 'Pompéia', 'Endoscopia', '16/12/2022 06:30');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `name`, `email`, `password`) VALUES
+(1, 'Rebeca Moreira Lopes', 'rebeca@hotmail.com', 'abc123'),
+(2, 'Rebeca Moreira Lopes', 'RebecaLopes123@hotmail.com', 'rebeca123');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `consultas`
+--
+ALTER TABLE `consultas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Índices para tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `consultas`
+--
+ALTER TABLE `consultas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ README.md
@@ -0,0 +1,32 @@
+# <center>App E-Clinic</center>
+
+## 🚀 Tecnologias
+
+- JavaScript
+- React Native
+- MySQL
+- Node.JS
+- CSS
+- Expo
+
+## 💻 Projeto
+
+O app da E-Clinic foi desenvolvido para facilitar o atendimento de seus pacientes, é possível consultar as unidades, planos de saúde e informações de canais de atendimento. Agora ficou simples e rápido agendar consultas e exames.
+
+## 🔖 Layout
+
+![E-Clinic](/screenshots/img1.png "Home")
+
+![E-Clinic](/screenshots/img2.png "Convênio")
+
+![E-Clinic](/screenshots/img3.png "Unidades")
+
+![E-Clinic](/screenshots/img4.png "Telas de login")
+
+![E-Clinic](/screenshots/img5.png "Agendar Consulta")
+
+![E-Clinic](/screenshots/img6.png "Central de Atendimento")
+
+## 📝 Licença
+
+Esse projeto está sob a licença MIT.
